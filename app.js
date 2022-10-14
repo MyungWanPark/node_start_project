1f93bc3171092db9ec0c196f43098978f35a151f
parkmyungwan <mywanpark@weirdsector.co.kr>
2022-10-14 20:56:51 +0900

    app.use로 middleware 처음 실행

--- app.js
+++ app.js
@@ -3,10 +3,23 @@ const express = require('express');
 const app = express();
 app.set('port', process.env.PORT || 3000);
 
-app.get('/', (req, res) => {
-    res.send('Hello Express!');
+app.use((req, res, next) => {
+    console.log('모든 요청에서 미들웨어가 실행됩니다.')
+    next();
+});
+
+app.use('/', (req, res, next) => {
+    console.log('get 요청에서 미들웨어가 실행됩니다.')
+    next();
+}, (req, res) => {
+    throw new Error('에러는 에러 처리 미들웨어로 갑니다.');
+});
+
+app.use((err, req, res, next) => {
+    console.log(err);
+    res.status(500).send(err.message);
 });
 
 app.listen(app.get('port'), ()=> {
     console.log('✅', app.get('port'),'번 포트에서 대기중');
-})
+});
