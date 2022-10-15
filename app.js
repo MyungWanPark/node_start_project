cc4b186384a5b96c9fe4a4e8f9c18f70b6b20106
parkmyungwan <mywanpark@weirdsector.co.kr>
2022-10-15 12:57:04 +0900

    use express-session

--- app.js
+++ app.js
@@ -2,6 +2,8 @@ const express = require('express');
 const path = require('path');
 const dotenv = require('dotenv');
 const morgan = require('morgan');
+const cookieParser = require('cookie-parser');
+const session = require('express-session');
 
 dotenv.config();
 
@@ -13,6 +15,27 @@ app.use(morgan('dev'));
 app.use('/', express.static(path.join(__dirname, 'public')));
 app.use(express.json());
 app.use(express.urlencoded({extended: false}));
+// app.set('trust proxy', 1);
+app.use(cookieParser(process.env.COOKIE_SECRET));
+app.use(session({
+    resave: false,
+    saveUninitialized: true,
+    secret: process.env.COOKIE_SECRET,
+    cookie: {
+        // domain: 'localhost',
+        httpOnly: true,
+        secure: false,
+    },
+    name: 'session-cookie',
+}));
+
+/* app.set('trust proxy', 1) // trust first proxy
+app.use(session({
+  secret: process.env.COOKIE_SECRET,
+  resave: false,
+  saveUninitialized: true,
+  cookie: { secure: false }
+})) */
 
 app.use((req, res, next) => {
     console.log('모든 요청에서 미들웨어가 실행됩니다.')
