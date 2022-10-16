fa775a39d6bd4cbe08c0db6075fdecf26b838580
parkmyungwan <mywanpark@weirdsector.co.kr>
2022-10-16 20:31:41 +0900

    sequalize 로 mysql 연결 설정

--- app.js
+++ app.js
@@ -1,71 +1,43 @@
 const express = require('express');
 const path = require('path');
-const dotenv = require('dotenv');
 const morgan = require('morgan');
-const cookieParser = require('cookie-parser');
-const session = require('express-session');
+const nunjucks = require('nunjucks');
 
-dotenv.config();
-
-const indexRouter = require('./routes');
-const userRouter = require('./routes/user');
+const { sequelize } = require('./models');
 
 const app = express();
-
-app.set('port', process.env.PORT || 3000);
+app.set('port', process.env.PORT || 3001);
+app.set('view engine', 'html');
+nunjucks.configure('views', {
+    express: app,
+    watch: true,
+});
+sequelize.sync({force: false})
+    .then(() => {
+        console.log('✔️ 데이터 베이스 연결 성공');
+    })
+    .catch((err) => {
+        console.log(err);
+    });
 
 app.use(morgan('dev'));
-app.use('/', express.static(path.join(__dirname, 'public')));
+app.use(express.static(path.join(__dirname, 'public')));
 app.use(express.json());
 app.use(express.urlencoded({extended: false}));
-// app.set('trust proxy', 1);
-app.use(cookieParser(process.env.COOKIE_SECRET));
-app.use(session({
-    resave: false,
-    saveUninitialized: true,
-    secret: process.env.COOKIE_SECRET,
-    cookie: {
-        // domain: 'localhost',
-        httpOnly: true,
-        secure: false,
-    },
-    name: 'session-cookie',
-}));
-
-/* app.set('trust proxy', 1) // trust first proxy
-app.use(session({
-  secret: process.env.COOKIE_SECRET,
-  resave: false,
-  saveUninitialized: true,
-  cookie: { secure: false }
-})) */
-
-app.use('/', indexRouter);
-app.use('/user', userRouter);
 
 app.use((req, res, next) => {
-    console.log('모든 요청에서 미들웨어가 실행됩니다.')
-    next();
+    const error = new Error(`${req.method} ${req.url} 라우터가 없습니다.`);
+    error.status(404);
+    next(error);
 });
-
-app.use('/', (req, res, next) => {
-    console.log('get 요청에서 미들웨어가 실행됩니다.')
-    next();
-}
-/* , (req, res) => {
-    throw new Error('에러는 에러 처리 미들웨어로 갑니다.');
-} */
-);
-
-app.use((req, res, next) => {
-    res.status(404).send('not found!');
-})
 
 app.use((err, req, res, next) => {
-    console.log(err);
-    res.status(500).send(err.message);
+    res.locals.message = err.message;
+    res.local.error = process.env.NODE_ENV !== 'production' ? err : {};
+    res.status(err.status || 500);
+    res.render('error');
 });
 
-app.listen(app.get('port'), ()=> {
-    console.log('✅', app.get('port'),'번 포트에서 대기중');
+app.listen(app.get('port'), () => {
+    console.log(`✅ ${app.get('port')} 번 포트에서 대기 중`);
 });
