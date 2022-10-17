40a2ea21a42e27ef0852f1c4ac2c799eca1e3c4f
parkmyungwan <mywanpark@weirdsector.co.kr>
2022-10-17 22:16:50 +0900

    User model, Comment model 작성

--- app.js
+++ app.js
@@ -14,7 +14,7 @@ nunjucks.configure('views', {
 });
 sequelize.sync({force: false})
     .then(() => {
-        console.log('✔️ 데이터 베이스 연결 성공');
+        console.log('✔️  데이터 베이스 연결 성공');
     })
     .catch((err) => {
         console.log(err);
